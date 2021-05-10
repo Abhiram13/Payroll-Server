4611f004c61325a7085c6f479209c1edb6ef8376
Abhiram13 <abhiram131869@gmail.com>
2021-05-10 17:08:02 +0530

    added list of roles

--- methods/DBTables.cs
+++ methods/DBTables.cs
@@ -4,4 +4,18 @@ namespace Database {
       public const string EMPLOYEE = "employee";
       public const string LOGINS = "logins";
    }
+
+   static class Roles {
+      public const string ALL = "All";
+      public const string ADMIN = "Admin";
+      public const string HR = "HR";
+      public const string APP_DEV = "Application Developer";
+      public const string SEN_APP_DEV = "Senior Application Developer";
+      public const string CEO = "Chief Exceutive Officer";
+      public const string CTO = "Chief Technical Officer";
+      public const string TL = "Team Leader";
+      public const string SL = "Sales Manager";
+      public const string PM = "Product Manager";
+      public const string TELECALLER = "Tele Caller";
+   }
 }
